3dcb795145b065470799573212a092935a8c2610
Andrei <andrei.vacaru@runtimeverification.com>
2022-03-15 11:47:35 +0200

    Initial Commit

---
+++ WhistBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhistBuddy
+//
+//  Created by Andrei Vacaru on 15.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhistBuddy/WhistBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhistBuddyApp.swift
+//  WhistBuddy
+//
+//  Created by Andrei Vacaru on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhistBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
