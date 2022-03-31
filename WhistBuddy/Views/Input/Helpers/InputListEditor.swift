df0d96e0d094b3543895c184780e4fd7390e3176
Andrei <andrei.vacaru@runtimeverification.com>
2022-03-31 13:36:08 +0300

    show bid constraints on last picker

--- WhistBuddy/Views/Input/Helpers/InputListEditor.swift
+++ WhistBuddy/Views/Input/Helpers/InputListEditor.swift
@@ -13,10 +13,11 @@ struct InputListEditor: View {
     var hasBids: Bool
     var currentRound: Int
     @State private var input: [Round.Bid] = Round.default.bids
+    @State private var sum : Int = 0
 
     var body: some View {
         List {
-            ForEach(0..<players.count, id: \.self) { id in
+            ForEach(0..<players.count - 1, id: \.self) { id in
                 let index = (id + currentRound) % players.count
                 InputEditor(playerName: players[index].name, inputBid: $input[index], hand: round.hand)
                     .onAppear() {
@@ -30,7 +31,30 @@ struct InputListEditor: View {
                         }
                     }
             }
+            .onChange(of: input) { val in
+                sum = sumOfBids()
+            }
+
+            let index = (players.count - 1 + currentRound) % players.count
+            InputEditor(playerName: players[index].name, inputBid: $input[index], hand: round.hand, isConstrained: !hasBids, sum: sum)
+                .onAppear() {
+                    input[index] = round.bids[index]
+                }
+                .onChange(of: input) { val in
+                    sum = sumOfBids()
+                }
+                .onDisappear() {
+                    if (!hasBids) {
+                        round.bids[index] = input[index]
+                    } else {
+                        round.results[index] = input[index]
+                    }
+                }
         }
+    }
+    
+    func sumOfBids() -> Int {
+        return (input.reduce(0) {$0 + $1.rawValue})
     }
 }
 
--- WhistBuddy/Views/Input/InputEditor.swift
+++ WhistBuddy/Views/Input/InputEditor.swift
@@ -11,13 +11,16 @@ struct InputEditor: View {
     var playerName:String
     @Binding var inputBid: Round.Bid
     var hand: Hand
+    var isConstrained: Bool = false
+    var sum: Int = 0
     var body: some View {
         VStack{
- 
-            Text(playerName)
+             Text(playerName)
             Picker("Players", selection: $inputBid) {
-                ForEach(Round.Bid.allCases.filter{ $0.rawValue <= hand.rawValue }) { count in
-                    Text(String(count.rawValue)).tag(count)
+                ForEach(Round.Bid.allCases.filter{ $0.rawValue <= hand.rawValue}) { count in
+                    if (!isConstrained || hand.rawValue != sum + count.rawValue - inputBid.rawValue) {
+                        Text(String(count.rawValue)).tag(count)
+                    }
                 }
             }
             .pickerStyle(.segmented)
@@ -28,6 +31,6 @@ struct InputEditor: View {
 
 struct InputEditor_Previews: PreviewProvider {
     static var previews: some View {
-        InputEditor(playerName: "Andrei", inputBid: .constant(Round.Bid.zero), hand: Hand.eight)
+        InputEditor(playerName: "Andrei", inputBid: .constant(Round.Bid.zero), hand: Hand.one, isConstrained: true, sum: 0)
     }
 }
