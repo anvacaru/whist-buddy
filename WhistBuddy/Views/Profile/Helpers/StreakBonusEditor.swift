cfa9b352e84e3bf06cf74e51db798f9cb815421c
Andrei <andrei.vacaru@runtimeverification.com>
2022-04-05 00:14:28 +0200

    fix streak value required for bonus

--- WhistBuddy/Views/Profile/Helpers/StreakBonusEditor.swift
+++ WhistBuddy/Views/Profile/Helpers/StreakBonusEditor.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct StreakBonusEditor: View {
     @Binding var prefersBonus: Bool
+    var playerCount: Profile.PlayerCount
     var body: some View {
         Toggle(isOn: $prefersBonus) {
             VStack(alignment: .leading) {
                 Text("Enable Streak Bonus")
                     .font(.title3)
                     .bold()
-                Text("When a player gets a 5 streak win, he will get awarded. Same goes for losing.")
+                Text("When a player gets a \(playerCount.rawValue + 1) streak win, he will get awarded. Same goes for losing.")
                     .font(.caption)
                     .foregroundColor(.gray)
             }
@@ -25,6 +26,6 @@ struct StreakBonusEditor: View {
 
 struct StreakBonusEditor_Previews: PreviewProvider {
     static var previews: some View {
-        StreakBonusEditor(prefersBonus:.constant(true))
+        StreakBonusEditor(prefersBonus:.constant(true), playerCount: Profile.PlayerCount.five)
     }
 }
